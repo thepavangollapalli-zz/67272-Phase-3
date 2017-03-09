97c1a3a2a91ae2bbfef92e0839d46e5b92981720
Pavan Gollapalli <pavankgollapalli@gmail.com>
2017-03-09 15:05:08 -0500

    fixed bugs in starter tests

--- test/factories.rb
+++ test/factories.rb
@@ -13,13 +13,13 @@ FactoryGirl.define do
   factory :item_price do
     association :item
     price 1.00
-    start_date Date.today
+    start_date Date.current
     end_date nil
   end
 
   factory :purchase do
     association :item
     quantity 1
-    date Date.today
+    date Date.current
   end
 end
